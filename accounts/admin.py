26fba253801a3ad0270be02efc06088718fa82df
Harsh Agola <harsh@pop-os.localdomain>
2021-05-09 23:43:17 +0530

    Ready for deploy

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User , Calls
+from .models import User , Calls , Advisor
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Calls)
+admin.site.register(Advisor)
--- nlab/settings.py
+++ nlab/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,8 +96,12 @@ WSGI_APPLICATION = 'nlab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'NLAB',
+        'USER': "postgres",
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -172,3 +179,5 @@ JWT_AUTH = {
   'JWT_AUTH_HEADER_PREFIX': 'Bearer',
   'JWT_AUTH_COOKIE': None,
 }
+
+django_heroku.settings(locals())
